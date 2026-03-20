734b8db244605386541a06e3112014d903a18204
Woohyun Shin <cpu344@gmail.com>
2026-03-20 16:12:25 +0900

    Bump to 0.3-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "kr.pe.sinu.uranus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2-alpha"
+        versionCode = 3
+        versionName = "0.3-alpha"
     }
 
     buildTypes {
